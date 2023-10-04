aa25c6f4e9208c5cabc8240c880f14f0967d7508
julianecerqueira <98759083+julianecerqueira@users.noreply.github.com>
2023-10-03 22:56:36 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const botoes = document.querySelectorAll('.botao');
+
+const personagens = document.querySelectorAll(".personagens");
+
+
+botoes.forEach((botao, indice) => {
+    botoes.addEventListener("click", ()=> {
+
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+        
+        botao.classList.add("selecionado");
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+       personagemSelecionado.classList.remove("selecionado");
+
+        personagens[indece].classList.add("selecionado");
+
+
+ })
+})
+
+
+
+
+
